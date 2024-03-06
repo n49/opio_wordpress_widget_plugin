c4da06353b2232974a8b63912913b2c400881ba6
nilay49 <nilay@n49.com>
2024-03-06 11:15:21 -0500

    added console statement to check the jQuery load sequence

--- assets/js/jquery-noConflict.js
+++ assets/js/jquery-noConflict.js
@@ -1,6 +1,8 @@
 // Check if jQuery is already loaded
 if (typeof jQuery !== 'undefined') {
     // Use noConflict() to release the $ alias
+    console.log('jQuery is loaded.');
+
     jQuery.noConflict(true);
 } else {
     console.log('jQuery is not loaded.');
