0b0f3c91e7fa3868e7d86a7cbd2e3398970b6535
nilay49 <nilay@n49.com>
2025-11-20 13:28:09 -0500

    extracting schema from html and putting in site's head

--- includes/class-feed-shortcode.php
+++ includes/class-feed-shortcode.php
@@ -6,8 +6,12 @@ use WP_Opio_Reviews\Includes\Core\Core;
 
 class Feed_Shortcode {
 
+    private static $schema_scripts = array();
+
     public function __construct(Feed_Deserializer $feed_deserializer) {
         $this->feed_deserializer = $feed_deserializer;
+        // Hook into wp_head to output schemas
+        add_action('wp_head', array($this, 'output_schemas_to_head'), 1);
     }
 
     function custom_esc($str) {
@@ -18,6 +22,53 @@ class Feed_Shortcode {
         add_shortcode('opio_feed', array($this, 'init'));
     }
 
+    /**
+     * Extract JSON-LD schema scripts from HTML content
+     */
+    private function extract_schema_from_html($html) {
+        $schemas = array();
+        
+        // Pattern to match script tags with type="application/ld+json" and id="jsonldSchema"
+        // Handles attributes in any order and with single or double quotes
+        $pattern = '/<script[^>]*(?:id=["\']jsonldSchema["\'][^>]*type=["\']application\/ld\+json["\']|type=["\']application\/ld\+json["\'][^>]*id=["\']jsonldSchema["\'])[^>]*>(.*?)<\/script>/is';
+        
+        if (preg_match_all($pattern, $html, $matches, PREG_SET_ORDER)) {
+            foreach ($matches as $match) {
+                $schemas[] = $match[0]; // Full script tag
+            }
+        }
+        
+        return $schemas;
+    }
+
+    /**
+     * Remove schema scripts from HTML content
+     */
+    private function remove_schema_from_html($html) {
+        // Remove script tags with id="jsonldSchema" and type="application/ld+json"
+        // Handles attributes in any order and with single or double quotes
+        $pattern = '/<script[^>]*(?:id=["\']jsonldSchema["\'][^>]*type=["\']application\/ld\+json["\']|type=["\']application\/ld\+json["\'][^>]*id=["\']jsonldSchema["\'])[^>]*>.*?<\/script>/is';
+        $html = preg_replace($pattern, '', $html);
+        
+        // Also remove any stray head tags that might be in the content
+        // Remove opening <head> tags
+        $html = preg_replace('/<head[^>]*>/i', '', $html);
+        // Remove closing </head> tags
+        $html = preg_replace('/<\/head>/i', '', $html);
+        
+        return $html;
+    }
+
+    /**
+     * Output all collected schemas to the head
+     */
+    public function output_schemas_to_head() {
+        if (!empty(self::$schema_scripts)) {
+            foreach (self::$schema_scripts as $schema) {
+                echo $schema . "\n";
+            }
+        }
+    }
 
     public function init($atts) {
         if (get_option('opio_active') === '0') {
@@ -57,6 +108,15 @@ class Feed_Shortcode {
             $option = $review_option == "opio" ? "reviewFeed" : "allReviewFeed";
             $opio_handler = new Opio_Handler($biz_id, $option, $review_type, $org_id);
             $reviews = $opio_handler->get_business();
+
+            // Extract schema scripts from the feed HTML
+            $schemas = $this->extract_schema_from_html($reviews);
+            if (!empty($schemas)) {
+                // Store schemas to be output in head
+                self::$schema_scripts = array_merge(self::$schema_scripts, $schemas);
+                // Remove schemas from body content
+                $reviews = $this->remove_schema_from_html($reviews);
+            }
 
             // Wrap entire feed content with Nitropack exclusion wrapper
             echo '<div data-nitro-exclude="all" data-nitro-ignore="true" data-nitro-no-optimize="true" data-nitro-preserve-ws="true">';
